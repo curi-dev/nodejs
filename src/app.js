480104db43c23a0f700eae2063420becdc98b097
Tiago Curi <shopper.tiago@gmail.com>
2020-08-18 00:14:48 -0300

    Desafio concluido

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
+const { uuid, isUuid } = require('uuidv4')
 const express = require("express");
 const cors = require("cors");
+
 
 // const { uuid } = require("uuidv4");
 
@@ -11,23 +13,82 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+ 
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ message: "This ID is not valid." })
+  };
+  
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  const likes = repositories[repositoryIndex].likes;
+
+  const updatedRepository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  };
+
+  repositories[repositoryIndex] = updatedRepository;
+  
+  return response.json(updatedRepository);
+
 });
 
+
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  if (!isUuid(id)) {
+    return response.status(400).json({ message: "This ID is not valid." });
+  };
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs, likes } = request.body;
+  
+  if (!isUuid(id)) {
+    return response.status(400).json({ message: 'This ID is not valid.' });
+  };
+  
+  repositoryToUpdateLikes = repositories.find(repository => repository.id == id);
+    
+  repositoryToUpdateLikes.likes++;
+
+  return response.json(repositoryToUpdateLikes);
 });
 
 module.exports = app;
